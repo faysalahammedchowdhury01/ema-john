fa3a252be203d4c4e8c2c9b30fbf256eb1015f89
Faysal <74490913+faysalchowdhury99@users.noreply.github.com>
2022-02-16 14:42:23 +0600

    search functionality added

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,24 +1,24 @@
-import React, { useEffect, useState } from "react";
-import { toast } from "react-toastify";
-import Cart from "../Cart/Cart";
-import Products from "../Products/Products";
-import Search from "../Search/Search";
+import React, { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import Cart from '../Cart/Cart';
+import Products from '../Products/Products';
+import Search from '../Search/Search';
 
 const Shop = () => {
   // states
   const [products, setProducts] = useState([]);
   const [displayedProducts, setDisplayedProducts] = useState([]);
   const [cart, setCart] = useState(
-    JSON.parse(localStorage.getItem("cartEma")) || []
+    JSON.parse(localStorage.getItem('cartEma')) || []
   );
 
   // get data from localhost
-  localStorage.setItem("cartEma", JSON.stringify(cart));
+  localStorage.setItem('cartEma', JSON.stringify(cart));
 
   // get json data from api
   useEffect(() => {
     fetch(
-      "https://raw.githubusercontent.com/ProgrammingHero1/ema-john-simple-resources/master/fakeData/products.JSON"
+      'https://raw.githubusercontent.com/ProgrammingHero1/ema-john-simple-resources/master/fakeData/products.JSON'
     )
       .then((res) => res.json())
       .then((data) => {
@@ -43,12 +43,12 @@ const Shop = () => {
 
     // if doesn't exist product on cart then add product to cart
     if (!exist) {
-      productClone["quantity"] = 1;
+      productClone['quantity'] = 1;
       newCart.push(productClone);
 
       // toast
-      toast.success("Successfully added to cart!", {
-        position: "bottom-left",
+      toast.success('Successfully added to cart!', {
+        position: 'bottom-left',
         autoClose: 5000,
         hideProgressBar: false,
         closeOnClick: true,
@@ -75,7 +75,7 @@ const Shop = () => {
     const cartItemIndex = newCart.findIndex(
       (item) => item.key === cartItem.key
     );
-    if (action === "plus") {
+    if (action === 'plus') {
       newCart[cartItemIndex].quantity = newCart[cartItemIndex].quantity + 1;
     } else {
       if (newCart[cartItemIndex].quantity === 1) return;
@@ -95,19 +95,24 @@ const Shop = () => {
   };
 
   // handle search
-  const handleSearch = (search) => {};
+  const handleSearch = (search) => {
+    const searchedProducts = products.filter((product) =>
+      product.name.toUpperCase().includes(search.toUpperCase())
+    );
+    setDisplayedProducts(searchedProducts);
+  };
 
   return (
     <section>
-      <div className='container-fluid'>
-        <div className='row'>
-          <div className='col-12'>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12">
             <Search handleSearch={handleSearch} />
           </div>
-          <div className='col-lg-8'>
+          <div className="col-lg-8">
             <Products products={displayedProducts} addToCart={addToCart} />
           </div>
-          <div className='col-lg-4'>
+          <div className="col-lg-4">
             <Cart
               cart={cart}
               handleCartItemQuantity={handleCartItemQuantity}
